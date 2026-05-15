6001f31f9d36c0d5b4e761590c2a647d49e90cb6
Saddam Shah <saddamshah415@gmail.com>
2026-05-15 06:32:58 +0500

    services-page completed

--- portfolio_main/urls.py
+++ portfolio_main/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.home, name = 'home'),
     path('about/', views.about, name = 'about'),
+    path('services/', views.services, name = 'services'),
 ]
--- portfolio_main/views.py
+++ portfolio_main/views.py
@@ -5,3 +5,6 @@ def home(request):
 
 def about(request):
     return render(request, 'about.html')
+
+def services(request):
+    return render(request, 'services.html')
