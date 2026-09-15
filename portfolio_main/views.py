b7951f9d73dd6eff27961014f1b412c3efce4dc6
Saddam Shah <saddamshah415@gmail.com>
2026-09-16 03:37:57 +0500

    Updated Dynamic blogs on home page

--- portfolio_main/views.py
+++ portfolio_main/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render
+from blog.models import Blog
 
 def home(request):
-    return render(request, 'home.html')
+    latest_posts = Blog.objects.filter(status='published').order_by('-created_at')[:3]
+    return render(request,'home.html',{'latest_posts': latest_posts})
 
 def about(request):
     return render(request, 'about.html')
